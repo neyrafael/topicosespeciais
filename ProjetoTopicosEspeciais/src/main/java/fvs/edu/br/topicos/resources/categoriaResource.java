9c5da85f730ae0448f31835d08fbc734600761cb
neyrafael <neyrafaeljosino@gmail.com>
2018-09-18 20:12:17 -0300

    Implementação

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/categoriaResource.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/categoriaResource.java
@@ -1,28 +1,27 @@
 package fvs.edu.br.topicos.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fvs.edu.br.topicos.Services.CategoriaService;
 import fvs.edu.br.topicos.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class categoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Bombril");
-		Categoria cat2 = new Categoria(2, "Detergente");
+	@Autowired
+	private CategoriaService service;
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+				
+		return ResponseEntity.ok () .body (obj);
 	}
 	
 }
